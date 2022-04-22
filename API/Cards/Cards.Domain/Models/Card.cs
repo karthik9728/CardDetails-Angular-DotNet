e71cc773e894043f3c2bf7bdfb642596ec005f10
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-04-22 16:02:54 +0530

    Implementation of Backend With Database Connectivity

---
+++ API/Cards/Cards.Domain/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.Domain.Models
+{
+    public class Card
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string CardHolderName { get; set; } 
+        public string CardNumber { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+        public int CVC { get; set; }
+
+    }
+}
---
+++ API/Cards/Cards.Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Cards.Infrastructure.Data;
+using Cards.Infrastructure.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.Infrastructure.Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public BaseRepository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
